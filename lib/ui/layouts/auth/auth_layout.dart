590cb42ba57b48a86a930a5659350df66c8ba069
antet <antet.dev@gmail.com>
2023-11-08 00:12:18 -0500

    Change end spacer for CRLF and create login structure

--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AuthLayout extends StatelessWidget {
   const AuthLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Auth layout',
-        ),
+    return Scaffold(
+      body: ListView(
+        children: const [
+          _DesktopBody(),
+        ],
+      ),
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Container(
+            width: 400,
+            height: double.infinity,
+          )
+        ],
       ),
     );
   }
