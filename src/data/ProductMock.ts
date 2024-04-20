d9eacf88cbb9cf88e2e8c89a6a17599af93a0118
antet <antet.dev@gmail.com>
2024-04-20 13:57:26 -0500

    Refactor: Styles are separated from the ListProductPage component.

---
+++ src/data/ProductMock.ts
@@ -0,0 +1,85 @@
+import { TablePropsInterface } from '../components/shared/interfaces/TablePropsInterface';
+
+// TODO: Replace the following mock data with a call to the API
+export const products: TablePropsInterface = {
+  headers: [
+    { key: 'id', text: 'id' },
+    { key: 'name', text: 'Nombre' },
+    { key: 'detail', text: 'Detalle' },
+    { key: 'price', text: 'Precio' },
+    { key: 'stock', text: 'stock' },
+    { key: 'status', text: 'Estatus' },
+    { key: 'supplier', text: 'Proveedor' },
+    { key: 'actions', text: 'Acciones' },
+  ],
+  searchItem: {
+    key: 'name',
+    placeholder: 'Busca productos por nombre',
+  },
+  rows: [
+    {
+      id: 1,
+      name: 'computador',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+    {
+      id: 2,
+      name: 'agua',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+    {
+      id: 3,
+      name: 'aceite',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+    {
+      id: 4,
+      name: 'calcetas',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+    {
+      id: 5,
+      name: 'teclado',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+    {
+      id: 6,
+      name: 'monitor',
+      detail: 'detail',
+      price: 100,
+      stock: 10,
+      status: 'Active',
+      supplier: 'supplier',
+    },
+  ],
+  actions: [
+    {
+      type: 'edit',
+      redirectTo: '/product/edit/',
+    },
+    {
+      type: 'delete',
+      redirectTo: '/product/delete/',
+    },
+  ],
+};
--- src/pages/main/products/List/ListProductPage.tsx
+++ src/pages/main/products/List/ListProductPage.tsx
@@ -1,92 +1,11 @@
 import { Button, Grid } from '@mui/material';
-import { TableComponent } from '../../../../components/shared/table/TableComponent';
-import { RootLayout } from '../../RootLayout';
 import { Add } from '@mui/icons-material';
+
 import { containerStyles } from './ListProductStyles';
 
-// TODO: Replace the following mock data with a call to the API
-const products = {
-  headers: [
-    { key: 'id', text: 'id' },
-    { key: 'name', text: 'Nombre' },
-    { key: 'detail', text: 'Detalle' },
-    { key: 'price', text: 'Precio' },
-    { key: 'stock', text: 'stock' },
-    { key: 'status', text: 'Estatus' },
-    { key: 'supplier', text: 'Proveedor' },
-    { key: 'actions', text: 'Acciones' },
-  ],
-  searchItem: {
-    key: 'name',
-    placeholder: 'Busca productos por nombre',
-  },
-  rows: [
-    {
-      id: 1,
-      name: 'computador',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-    {
-      id: 2,
-      name: 'agua',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-    {
-      id: 3,
-      name: 'aceite',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-    {
-      id: 4,
-      name: 'calcetas',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-    {
-      id: 5,
-      name: 'teclado',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-    {
-      id: 6,
-      name: 'monitor',
-      detail: 'detail',
-      price: 100,
-      stock: 10,
-      status: 'Active',
-      supplier: 'supplier',
-    },
-  ],
-  actions: [
-    {
-      type: 'edit',
-      redirectTo: '/product/edit/',
-    },
-    {
-      type: 'delete',
-      redirectTo: '/product/delete/',
-    },
-  ],
-};
+import { RootLayout } from '../../RootLayout';
+import { TableComponent } from '../../../../components';
+import { products } from '../../../../data/ProductMock';
 
 export const ListProductPage = () => {
   return (
