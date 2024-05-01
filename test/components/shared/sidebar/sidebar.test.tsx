f582944c1f510eb7529b4130c5be10a669ddbd64
antet <antet.dev@gmail.com>
2024-05-01 00:05:40 -0500

    Remove commented out test case in sidebar.test.tsx

--- test/components/shared/sidebar/sidebar.test.tsx
+++ test/components/shared/sidebar/sidebar.test.tsx
@@ -16,18 +16,4 @@ describe('NavbarComponent', () => {
     const { getByTestId } = render(<SidebarComponent state={true} />);
     expect(getByTestId('sidebar')).not.toBeVisible();
   });
-
-  // test('should toggle sidebar state with click on navbar', async () => {
-  //   const setOpen = vi.fn();
-  //   const { getByTestId } = render(
-  //     <MemoryRouter initialEntries={['']}>
-  //       <NavbarComponent setOpen={setOpen} state={false} />
-  //       <SidebarComponent state={false} />
-  //     </MemoryRouter>
-  //   );
-
-  //   expect(getByTestId('sidebar')).toBeVisible();
-  //   userEvent.click(getByTestId('menu-button'));
-  //   expect(await getByTestId('sidebar')).not.toBeVisible();
-  // });
 });
