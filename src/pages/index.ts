9350736fe744f902e40f1c10f954b1ac1650760c
antet <antet.dev@gmail.com>
2024-05-01 18:08:31 -0500

    Change: renamed all pages in main module.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,19 +4,19 @@ export { RecoveryPage } from './auth/recovery/recovery.page';
 
 // Main module
 export { RootPage } from './main/root.page';
-export { HomePage } from './main/home/HomePage';
+export { HomePage } from './main/home/home.page';
 
 // User module
-export { ListUserPage } from './main/users/List/ListUserPage';
-export { CreateUserPage } from './main/users/create/CreateUserPage';
-export { UpdateUserPage } from './main/users/update/UpdateUserPage';
+export { ListUserPage } from './main/users/List/list-user.page';
+export { CreateUserPage } from './main/users/create/create-user.page';
+export { UpdateUserPage } from './main/users/update/update-user.page';
 
 // Product module
 export { ListProductPage } from './main/products/List/list-product.page';
 export { CreateProductPage } from './main/products/create/create-product.page';
-export { UpdateProductPage } from './main/products/update/UpdateProductPage';
+export { UpdateProductPage } from './main/products/update/update-product.page';
 
 // Customer module
-export { ListCustomerPage } from './main/customer/List/ListCustomerPage';
-export { CreateCustomerPage } from './main/customer/create/CreateCustomerPage';
-export { UpdateCustomerPage } from './main/customer/update/UpdateCustomerPage';
+export { ListCustomerPage } from './main/customer/List/list-customer.page';
+export { CreateCustomerPage } from './main/customer/create/create-customer.page';
+export { UpdateCustomerPage } from './main/customer/update/update-customer.page';
