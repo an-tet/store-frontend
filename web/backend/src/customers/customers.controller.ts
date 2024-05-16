1a4f89c5f98150a6ec2e9780b880e2a37cd8cf82
antet <antet.dev@gmail.com>
2024-05-15 21:19:20 -0500

    Add: auth validations in customer module.

--- web/backend/src/customers/customers.controller.ts
+++ web/backend/src/customers/customers.controller.ts
@@ -13,27 +13,33 @@ import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/enum/valid-roles';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
   @Post()
+  @Auth(ValidRoles.admin, ValidRoles.seller)
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customersService.create(createCustomerDto);
   }
 
   @Get()
+  @Auth(ValidRoles.admin, ValidRoles.seller)
   findAll(@Query() paginationDto: PaginationDto) {
     return this.customersService.findAll(paginationDto);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin, ValidRoles.seller)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.customersService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin, ValidRoles.seller)
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCustomerDto: UpdateCustomerDto,
@@ -42,6 +48,7 @@ export class CustomersController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin, ValidRoles.seller)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.customersService.remove(id);
   }
