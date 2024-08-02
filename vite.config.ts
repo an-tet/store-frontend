a93279f93b76478b1bfda13034208ce016837077
name <antet.dev@gmail.com>
2024-08-01 21:10:27 -0500

    chore: Refactor vite.config.ts to remove unused HMR configuration and update proxy configuration for '/api'

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }: { mode: string }) => {
   const env = loadEnv(mode, '', '');
   return {
-    hmr: false,
     server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3001',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
       host: true,
       strictPort: true,
       port: parseInt(env.VITE_SERVER_PORT),
